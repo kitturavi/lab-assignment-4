5f38f5ea569b91820b935dae1f8f8cc75cf9fa7c
kitturavi <ravi.kit1602@gmail.com>
2022-01-14 03:24:52 +0530

    q1
    push

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 50;
+            while (i >= 1)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
